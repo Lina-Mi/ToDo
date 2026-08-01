e0a58c70427033efd3130e90fa0c651aa9af8a7b
Carol <cartochka888@gmail.com>
2026-08-01 05:51:05 +0300

    styles and text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
 
 export const App = () => {
+	const [todos, setTodos] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		setIsLoading(true);
+
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((response) => response.json())
+			.then((loadedTodos) => {
+				setTodos(loadedTodos);
+			})
+			.finally(() => setIsLoading(false));
+	}, []);
+
 	return (
-		<div className="App">
-			<div>{}</div>
+		<div className={styles.App}>
+			<h1 className={styles.title}>To Do List</h1>
+			<div className={styles.todoList}>
+				{isLoading ? (
+					<div className={styles.loader}></div>
+				) : (
+					todos.map(({ id, title }) => (
+						<div className={styles.todoItem} key={id}>
+							<div className={styles.todoId}>{id}</div>
+							<div className={styles.todoTitle}>{title}</div>
+						</div>
+					))
+				)}
+			</div>
 		</div>
 	);
 };
